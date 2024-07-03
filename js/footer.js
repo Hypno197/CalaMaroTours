2359ea95954c95359855d91cf6cf5bc83e2801d1
Luca <luca_barreca@hotmail.it>
2024-07-03 18:32:48 +0200

    merge

--- js/footer.js
+++ js/footer.js
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Footer Cala Marò</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="StyleGabriel.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-</head>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <footer class="footer text-center text-lg-start">
+const template2 =document.createElement('template')
+template.innerHTML =`
+<footer class="footer text-center text-lg-start">
 
         <div class="container container-footer p-4 pb-0">
             
@@ -77,6 +65,6 @@
         <div class="container text-center mt-3">
             <p>&copy; 2024 Cala Marò TO - S.r.l. Tutti i diritti riservati.</p>
         </div>
-    </footer>
-
-</html>
+        </footer>
+`
+document.body.appendChild(template.content)
