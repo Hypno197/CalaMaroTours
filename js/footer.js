36ce204f7dc3d34498b23bd5f7447faec5060bed
Luca <luca_barreca@hotmail.it>
2024-07-11 09:15:46 +0200

    preventivoV0.0.1

--- js/footer.js
+++ js/footer.js
@@ -1,23 +1,23 @@
 const template2 =document.createElement('template')
 template.innerHTML =`
-<footer>
+<footer class="footer mt-auto py-3">
 
-        <div class="container text-center">
+        <div class="container-fluid text-center">
             
             <div class="row">
 
-                <div style="margin-left: -7%;" class="col-12 col-md-1 footer-col">
+                <div class="col-12 col-lg-1 footer-col">
                     <a href="Home.html">
                         <img style="border-radius: 50%;" src="img/Logo.png" alt="Logo Cala Marò" width="100px" height="100px">
                     </a>
                 </div>
 
-                <div class="col-12 col-md-2 footer-col">
+                <div class="col-12 col-sm-6 col-lg-2 footer-col">
                     <span class="text-uppercase mb-4 font-weight-bold text-center spanG">Dati aziendali</span>
                     <p>Nome Azienda: <b>Cala Marò TO - S.r.l.</b></p>
                     <p>Indirizzo Sede Legale: <b>Via Manfredonia, 1 - 71121 Foggia (FG)</b></p>
                 </div>
-                <div class="col-12 col-md-3 footer-col">
+                <div class="col-12 col-sm-6 col-lg-3 footer-col">
                     <span class="text-uppercase mb-4 font-weight-bold text-center spanG">Contatti azienda</span>
                     <p>Telefono: <b>+39 123-456-7890</b></p>
                     <p>E-mail: <b>azienda-to@calamaro.it</b></p>
@@ -25,22 +25,22 @@ template.innerHTML =`
 
                 
 
-                <div class="col-12 col-md-3 footer-col">
+                <div class="col-12 col-lg-3 footer-col">
                     <div class="row">
                         <div class="col text-center ms-3">
                             <h4>Help section</h4>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-12 col-md-6">                      
+                        <div class="col-12 col-sm-6 col-lg-6">                      
                     <ul class="link-list">
                         <li><a href="FAQ.html" class="text-white">FAQ</a></li>
                         <li><a href="ChiSiamo.html" class="text-white">Chi siamo</a></li>
-                        <li><a href="Contatti.html" class="text-white">Contatti</a></li>
-                        <li><a href="LavoraConNoi.html" class="text-white">Lavora con noi</a></li>
+                        <li><a href="Contatti2.html" class="text-white">Contatti</a></li>
+                        <li><a href="Contatti2.html" class="text-white">Lavora con noi</a></li>
                     </ul>
                         </div>
-                        <div class="col-12 col-md-6">
+                        <div class="col-12 col-sm-6 col-lg-6">
                             <ul class="link-list">
                                 <li><a href="Assistenza.html" class="text-white">Assistenza</a></li>
                                 <li><a href="CondizioniGenerali.html" class="text-white">Condizioni generali</a></li>
@@ -51,7 +51,7 @@ template.innerHTML =`
                     </div>
                 </div>
 
-                <div style="margin-right: -7%;" class="col-12 col-md-3">
+                <div class="col-12 col-sm- col-lg-3">
             
                 <div class="row">
                     <h4>Social</h4>
